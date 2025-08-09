962fa12afabaae5b295e4c33b1627a961527ddcd
Kabeer021 <124332571+Kabeer021@users.noreply.github.com>
2025-08-09 12:23:22 +0530

    Update main.js

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ if(localStorage.getItem("bestBrain")){
 
 const traffic = [
     new Car(road.getLaneCenter(0) ,-100,30,50,"DUMMY",2),
-    new Car(road.getLaneCenter(1) ,-500,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(2) ,-500,30,50,"DUMMY",2),
     new Car(road.getLaneCenter(2) ,-100,30,50,"DUMMY",2),
     new Car(road.getLaneCenter(0) ,-1000,30,50,"DUMMY",2),
     new Car(road.getLaneCenter(0) ,-600,30,50,"DUMMY",2),
@@ -109,4 +109,5 @@ function animate(time)
     Visualizer.drawNetwork(networkCtx , bestCar.brain);
     requestAnimationFrame(animate);
 }
+
 
